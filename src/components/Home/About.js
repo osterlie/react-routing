bc9e5cc668151dd69fa46bb8895f968a9d57674a
osterlie <67156628+osterlie@users.noreply.github.com>
2020-09-07 19:28:03 +0000

    first commit

---
+++ src/components/Home/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h3>This is About Us</h3>
+            <p>Lorem ipsum dolor sit amet, in eos dicant putant, quo no dolorum forensibus instructior. Suas wisi ne his, modo alienum delicata est et, eu his graeco oportere. Per te inani numquam, iisque gubergren ullamcorper sed ne. Saepe tation admodum te eum, nam eu erat everti bonorum. Putant habemus cum ei. <br/><br/>
+
+            Est ei utamur fuisset, mea in utinam pertinacia. Te eum mandamus quaerendum, cum at quod partiendo. Ex per noster appellantur, eam vitae forensibus id. Sit corpora expetenda quaerendum eu, eam inani euismod intellegebat et. Vim esse iudico nonumes an, an iisque vocibus iudicabit sea.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home/Shishi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import pictureim from '../Home/pictureim.png'
+
+function Shishi() {
+    return (
+        <div>
+        <img src={pictureim} style={{ width: '500px', margin: 'auto', display: 'block' }} alt="Router" />
+        </div>
+    )
+}
+
+export default Shishi
